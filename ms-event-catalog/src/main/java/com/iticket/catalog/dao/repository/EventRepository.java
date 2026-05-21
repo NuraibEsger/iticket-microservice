7d67e4f711b574f9080f33da5ad34e2324286167
nuraibesger <nuraibesger@gmail.com>
2026-05-21 21:51:27 +0400

    Integer capacity field added to VenueEntity

--- ms-event-catalog/src/main/java/com/iticket/catalog/dao/repository/EventRepository.java
+++ ms-event-catalog/src/main/java/com/iticket/catalog/dao/repository/EventRepository.java
@@ -4,8 +4,6 @@ import com.iticket.catalog.dao.entity.EventEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.math.BigDecimal;
-
 @Repository
 public interface EventRepository extends JpaRepository<EventEntity,Long> {
 }
